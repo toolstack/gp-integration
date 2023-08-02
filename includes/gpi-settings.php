40516c8c726396e1094778ddc808b5ac0bb9a4dc
Alex Lion <learnwithalex@gmail.com>
2023-08-02 14:03:55 -0700

    Improve I18N Issues (Based on 1.5)

--- includes/gpi-settings.php
+++ includes/gpi-settings.php
@@ -4,10 +4,10 @@
 		
 		$ret = array();
 		
-		$ret['gp_database_name']							= array( 'type' => 'text', 'desc' => 'GlotPress DB Name', 'setting' => htmlentities( $gpi_utils->get_option('gp_database_name') ) );
-		$ret['gp_table_prefix'] 							= array( 'type' => 'text', 'desc' => 'GlotPress DB Prefix', 'size' => 5, 'setting' => htmlentities( $gpi_utils->get_option('gp_table_prefix') ) );
-		$ret['gp_path'] 									= array( 'type' => 'text', 'desc' => 'GlotPress Path', 'size' => 60, 'setting' => htmlentities( $gpi_utils->get_option('gp_path') ) );
-		$ret['gp_dir']										= array( 'type' => 'text', 'desc' => 'GlotPress Directory', 'size' => 60, 'setting' => htmlentities( $gpi_utils->get_option('gp_dir') ) );
+		$ret['gp_database_name']							= array( 'type' => 'text', 'desc' => __( 'GlotPress DB Name', 'gp-integration'), 'setting' => htmlentities( $gpi_utils->get_option('gp_database_name') ) );
+		$ret['gp_table_prefix'] 							= array( 'type' => 'text', 'desc' => __( 'GlotPress DB Prefix', 'gp-integration'), 'size' => 5, 'setting' => htmlentities( $gpi_utils->get_option('gp_table_prefix') ) );
+		$ret['gp_path'] 									= array( 'type' => 'text', 'desc' => __( 'GlotPress Path', 'gp-integration'), 'size' => 60, 'setting' => htmlentities( $gpi_utils->get_option('gp_path') ) );
+		$ret['gp_dir']										= array( 'type' => 'text', 'desc' => __( 'GlotPress Directory', 'gp-integration'), 'size' => 60, 'setting' => htmlentities( $gpi_utils->get_option('gp_dir') ) );
 
 		return $ret;
 	}
